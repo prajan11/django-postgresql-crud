1a59692688ece13621458876e72cf25e1f26056c
prajan11 <chasmis123>
2019-12-10 15:24:10 +0545

    Form Implementation Using CrispyForms, Post Request Handling to add Employee Records

---
+++ employee_register/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Employee
+
+class EmployeeForm(forms.ModelForm):
+
+    class Meta:
+        model = Employee     
+        fields = ('fullname', 'mobile', 'emp_code', 'position')
+        labels = {
+            'fullname': 'Full Name',
+            'mobile': 'Mobile Number',
+            'emp_code': "Employee Code",
+            'position': "Position",
+        }
+
+
+    def __init__(self, *args, **kwargs):
+        super(EmployeeForm, self).__init__(*args, **kwargs)
+        self.fields['position'].empty_label = "Select Position"
+        self.fields['emp_code'].required = False
--- employee_register/views.py
+++ employee_register/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import EmployeeForm
+from .models import Employee
 
 # Create your views here.
+def employee_list(request):
+    context = {'employee_list':Employee.objects.all()}
+    return render(request,"employee_register/employee_list.html",context)
+
+def employee_form(request):
+    if request.method == "GET":
+        form = EmployeeForm()
+        return render(request,"employee_register/employee_form.html",{'form':form})
+    else:
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/employee/list")
+
+
+def employee_delete(request):
+    return
