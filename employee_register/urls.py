691465be0da43647f7cabc947182ef43e02a4eae
prajan11 <chasmis123>
2019-12-10 16:27:29 +0545

    Implementation of delete operation

--- employee_register/urls.py
+++ employee_register/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.employee_form, name="employee_insert"),
     path('<int:id>/',views.employee_form, name="employee_update"),
     path('list/', views.employee_list, name="employee_list"),
+    path('delete/<int:id>', views.employee_delete, name='employee_delete')
 ]
