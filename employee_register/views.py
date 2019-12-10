41b7686b3f7a2f39d5a7de077b2766915b8ae5c2
prajan11 <chasmis123>
2019-12-10 15:46:53 +0545

    Implementation of update operation

--- employee_register/views.py
+++ employee_register/views.py
@@ -7,14 +7,25 @@ def employee_list(request):
     context = {'employee_list':Employee.objects.all()}
     return render(request,"employee_register/employee_list.html",context)
 
-def employee_form(request):
+def employee_form(request, id=0):
     if request.method == "GET":
-        form = EmployeeForm()
+        if id == 0:
+            form = EmployeeForm()
+        else:
+            employee = Employee.objects.get(id=id)
+            form = EmployeeForm(instance=employee)
+
         return render(request,"employee_register/employee_form.html",{'form':form})
     else:
-        form = EmployeeForm(request.POST)
+        if id == 0:
+            form = EmployeeForm(request.POST)
+        else:
+            employee = Employee.objects.get(id=id)
+            form = EmployeeForm(request.POST, instance = employee)
+
         if form.is_valid():
             form.save()
+
         return redirect("/employee/list")
 
 
